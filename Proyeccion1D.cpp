969e2c42dfca0028345beeb97d384b1a330eea3c
alexjr2001 <63054183+alexjr2001@users.noreply.github.com>
2023-04-18 18:57:49 -0500

    Update Proyeccion1D.cpp

--- Proyeccion1D.cpp
+++ Proyeccion1D.cpp
@@ -1,10 +1,19 @@
 /*
-PROYECTO: PROYECCIONES DE 10D A 1D
-AUTOR: ALEXANDER SEBASTI�N G�MEZ DEL CARPIO
-FECHA: 30/06/2022
+PROJECT: Projection MultiDimension to 1D
+AUTHOR: ALEXANDER SEBASTIAN GOMEZ DEL CARPIO
+DATE: 06/30/2022
+
+[EN]
+IN THIS CODE THE CONVERSION OF DATA FROM 10 DIMENSIONS TO 1 DIMENSION IS FOUND, IT IS TAKEN AS
+THE Z-ORDER CRITERION. THEN, HAVING CONVERTED THESE VALUES, ONE IS CHOOSE, WHICH WILL BE COMPARED TO
+SEARCH YOUR 5, 10, 15 AND 20 NEAREST NEIGHBORS IN 10 DIMENSIONS AND IN ONE DIMENSION. FINALLY, WE RETURN
+THE PERCENTAGE OF COINCIDENCE BETWEEN THESE TWO PROJECTIONS.
 
 [ES]
-EN EL PRESENTE CODIGO SE ENCUENTRA LA CONVERSION DE DATOS DE 10 DIMENSIONES A 1 DIMENSION, SE TOMA COMO CRITERIO EL Z-ORDER. LUEGO, DE HABER CONVERTIDO ESTOS VALORES, SE ELIGE UNO, EL CUAL SERÁ COMPARADO PARA BUSCAR SUS 5, 10, 15 Y 20 VECINOS MAS CERCANOS EN 10 DIMENSIONES Y EN UNA DIMENSION. FINALMENTE, DEVOLVEMOS EL PORCENTAJE DE COINCIDENCIA ENTRE ESTAS DOS PROYECCIONES.
+EN EL PRESENTE CODIGO SE ENCUENTRA LA CONVERSION DE DATOS DE 10 DIMENSIONES A 1 DIMENSION, SE TOMA COMO 
+CRITERIO EL Z-ORDER. LUEGO, DE HABER CONVERTIDO ESTOS VALORES, SE ELIGE UNO, EL CUAL SERÁ COMPARADO PARA 
+BUSCAR SUS 5, 10, 15 Y 20 VECINOS MAS CERCANOS EN 10 DIMENSIONES Y EN UNA DIMENSION. FINALMENTE, DEVOLVEMOS 
+EL PORCENTAJE DE COINCIDENCIA ENTRE ESTAS DOS PROYECCIONES.
 */
 
 #include <iostream>
@@ -16,7 +25,7 @@ EN EL PRESENTE CODIGO SE ENCUENTRA LA CONVERSION DE DATOS DE 10 DIMENSIONES A 1 
 
 using namespace std;
 
-//CALCULA LA DISTANCIA EUCLIDIANA
+//Calculates the euclidean distance
 int distance(vector<int> v1, vector<int> v2){
 	int suma=0;
 	for(int i=0; i<v1.size();i++){
@@ -26,7 +35,7 @@ int distance(vector<int> v1, vector<int> v2){
 	return suma;
 }
 
-//VOLTEA UN NUMERO BINARIO (110->011)
+//Turns backwards a binary number (110->011)
 void reverse(string& str)
 {
     int n = str.length();
@@ -34,7 +43,7 @@ void reverse(string& str)
         swap(str[i], str[n - i - 1]);
 }
 
-//CONVERTIMOS UN NUMERO DECIMAL A UNO BINARIO
+//Converts a decimal number to a binary one
 string convert(int n, int tam=0){
     string r;
     while (n != 0){
@@ -52,7 +61,7 @@ string convert(int n, int tam=0){
     return r1;
 }
 
-//CONVERTIMOS UN NUMERO BINARIO A UN DECIMAL
+//Converts a binary number to a decimal one
 long long convertBinLL(string bin){
 		long long result=0;
 		for(int i=bin.size()-1,j=0;i>=0;i--,j++){
@@ -63,7 +72,7 @@ long long convertBinLL(string bin){
     return result;
 }
 
-//CONVERTIMOS UN VECTOR DE 10 DIMENSIONES A UN STRING DE 1D CON LOS BITS REPARTIDOS
+//Converts a vector of 10 dimensions to a string of 1 dimension with the bits distributed
 string vecBin(vector<int> v, int tam, vector<string> bin){
 	string D1;  
 	for(int i=0; i<tam;i++){
@@ -75,24 +84,24 @@ string vecBin(vector<int> v, int tam, vector<string> bin){
 }
 
 int main() {
-	//FUNCION RANDOM
+	//Random function
 	std::random_device rd;  
 	std::mt19937 gen(rd()); 
 	std::uniform_int_distribution<> distrib(0, 9);
 
-	int data=1000;			//TOMAMOS 1000 DATOS PARA QUE HAYA ALGUNAS COINCIDENCIAS, CON 5000 ES DIFICIL QUE SE ENCUENTREN
-	int lim=10;					//LOS VALORES SON DEL 0 AL 9
+	int data=1000;			//We take 1000 numbers as data, thus that there are some coincidences( with 5000 it is difficult to find coincidences)
+	int lim=10;					//The values are from 0 to 9
 	int dim=10;					//DIMENSION
 	vector<vector<int>> data10D(data,vector<int>(dim));
 	
-	for(int i=0; i<data;i++){				//CREAMOS LOS DATOS RANDOM
+	for(int i=0; i<data;i++){				//Creates the random data
 		for(int j=0;j<dim;j++){		
 			data10D[i][j]=distrib(gen);			
 		}
 	}
 
 	
-	vector<string> bin;										//GUARDAMOS DEL 0 AL 9 EN BINARIOS
+	vector<string> bin;										//Saves from 0 to 9 in binaries
 	int tamBits = convert(lim-1).size();
 	for(int i=0;i<lim-1;i++){
 		bin.push_back(convert(i,tamBits));
@@ -102,7 +111,7 @@ int main() {
 
 	vector<pair<vector<int>*,int>>::iterator it;
 	vector<pair<vector<int>*,int>> knn10D;
-	int pos=1;								//POSICION DEL DATO EN 10D ELEGIDO
+	int pos=1;								//Position of data in 10D choosen
 	vector<int> choosen=data10D[pos];
 
 	
@@ -111,11 +120,11 @@ int main() {
 	bool find=false;
 	for(int i=0;i<data;i++){
 		if(i!=pos){
-			dist=distance(choosen,data10D[i]);				//HALLAMOS SU DISTANCIA
+			dist=distance(choosen,data10D[i]);				//Finds out the distance
 			find=false;
 			for(it=knn10D.begin();it!=knn10D.end();++it){
 				if(it->second>=dist){
-					knn10D.insert(it, make_pair(&data10D[i],dist));			//INSERTAMOS EN ORDEN CRECIENTE SUS KVECINOS
+					knn10D.insert(it, make_pair(&data10D[i],dist));			//INSERT ITS K-NEIGHBORS IN INCREASING ORDER
 					find=true;
 					break;
 				}
@@ -129,18 +138,18 @@ int main() {
 	vector<pair<vector<int>*,long long>>::iterator it2;
 	vector<pair<vector<int>*,long long>> knn1D;
 	it2=knn1D.begin();
-	string binChoosen=vecBin(choosen,tamBits,bin); 		//EL DATO DE 10D ELEGIDO LO CONVERTIMOS EN 1D CON Z-ORDER
+	string binChoosen=vecBin(choosen,tamBits,bin); 		//WE CONVERT THE 10D DATA CHOSEN INTO 1D WITH Z-ORDER
 	long long pos1D=convertBinLL(binChoosen);			
 
 	find=false;	
 	long long dist2;
 	for(int i=0;i<data;i++){
 		if(i!=pos){
-			dist2=abs(pos1D-convertBinLL(vecBin(data10D[i],tamBits,bin)));		//CONVERTIMOS EL DATO A COMPARAR A 1D Y CALCULAMOS SU DISTANCIA
+			dist2=abs(pos1D-convertBinLL(vecBin(data10D[i],tamBits,bin)));		//WE CONVERT THE DATA TO BE COMPARED TO 1D AND CALCULATE ITS DISTANCE
 			find=false;
 			for(it2=knn1D.begin();it2!=knn1D.end();++it2){
 				if(it2->second>=dist2){
-					knn1D.insert(it2, make_pair(&data10D[i],dist2));			//INSERTAMOS EN ORDEN CRECIENTE SUS KVECINOS
+					knn1D.insert(it2, make_pair(&data10D[i],dist2));			//INSERT ITS K-NEIGHBORS IN INCREASING ORDER
 					find=true;
 					break;
 				}
@@ -151,7 +160,7 @@ int main() {
 		}
 	}
 
-	//HALLAMOS SI COINCIDEN LOS VECINOS MAS CERCANOS
+	//We find out if the K-Neighbours have any coincidence
 	bool found=false;
 	int cantidad[4]={5,10,15,20};
 	int match[4]={5,10,15,20};
@@ -175,7 +184,7 @@ int main() {
 		}
 	}
 
-	//IMPRIMIMOS
+	//Print out the neighbours, then which ones match and finally, the percentage of the matching
 	cout<<"VECINOS:  ";
 	for(int i=0;i<4;i++){
 		cout<< cantidad[i] << " ";
